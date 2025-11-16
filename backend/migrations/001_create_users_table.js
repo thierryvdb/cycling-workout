0f606ccf63176e9f2c074e0e6c4759482d1cc2c0
thierryvdb <cloudyvdb@gmail.com>
2025-11-16 17:08:37 -0300

    go go

--- backend/migrations/001_create_users_table.js
+++ backend/migrations/001_create_users_table.js
@@ -1,4 +1,4 @@
-const db = require('../../config/database');
+const db = require('../config/database');
 
 async function createUsersTable() {
   const query = `
--- backend/migrations/002_create_workouts_tables.js
+++ backend/migrations/002_create_workouts_tables.js
@@ -1,4 +1,4 @@
-const db = require('../../config/database');
+const db = require('../config/database');
 
 async function createWorkoutsTables() {
   const workoutsQuery = `
